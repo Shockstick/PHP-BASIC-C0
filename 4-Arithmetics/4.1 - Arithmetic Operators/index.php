223367f3380c3e369b3c360f911b65142cf373e7
Shockstick <2152301@talnet.nl>
2022-01-28 09:46:35 +0100

    Fix

--- 4-Arithmetics/4.1 - Arithmetic Operators/index.php
+++ 4-Arithmetics/4.1 - Arithmetic Operators/index.php
@@ -6,7 +6,15 @@
 
 $getal1 = 50;
 $getal2 = 3;
-    echo $getal1 * $getal2;
+$getal3 = 20;
+$getal4 = 4;
+    echo $getal1 * $getal2; 
+    echo "<br>";
+    echo $getal1 - $getal3; 
+    echo "<br>";
+    echo $getal3 / $getal4;
+    echo "<br>";
+    echo $getal2 + $getal4; 
 ?>
 </body>
 </html>
